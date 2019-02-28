71c25523e1db4318c2d99f0512d33e046eaab944
Renata Tavares <renatattavares@hotmail.com>
2019-02-28 10:16:04 -0300

    Added preprocessor

---
+++ mspreprocessor/msCoarseningLib/__init__.py
@@ -0,0 +1,8 @@
+#Multiscale preprocessors for reservoir simulation using PRESTO.
+
+print('Partitioning module initialized')
+
+__all__ = ["readConfig","algoritmo"]
+#import meshManager
+
+print(__name__)
