2c33c1680d916605bfe0713b3db0b2070bb8125f
Renata Tavares <renatattavares@hotmail.com>
2019-11-29 19:11:38 -0300

    Extended local script

---
+++ upscaling_procedures/extended_local_upscaling.py
@@ -0,0 +1,10 @@
+"""
+Module of extended-local upscaling technique in structured tridimensional meshes
+"""
+import time
+import numpy as np
+
+class ExtendedLocalUpscaling(LocalUpscaling):
+
+    def __init__(self):
+        pass
