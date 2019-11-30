3de3cc8bc97d2d90e6a69df63af78cc4e4179916
Renata Tavares <renatattavares@hotmail.com>
2019-11-30 11:30:13 -0300

    Coarse volumes centers of structured meshes caculated

--- upscaling_procedures/local_upscaling.py
+++ upscaling_procedures/local_upscaling.py
@@ -19,25 +19,31 @@ class LocalUpscaling:
         -> Solve global coarse problem
         """
 
+        print('Local uscaling class initialized')
+
         # Preprocessing mesh with IMPRESS
         start = time.time()
         self.mesh = preprocessor(mesh_file, dim = 3) # IMPRESS' object
         end = time.time()
-        print("The preprocessing step lasted {0}s".format(end-start))
-
-        # Coarsening informations
-        self.coarse_config = coarse_config()
-        self.tree = self.coarse_config.tree['Simple']
-        self.get_coarse_informations()
-        print('Coarsening informations accessed')
+        print("\nThe preprocessing step lasted {0}s".format(end-start))
 
         # Setting variables
         self.boundary_condition_type = boundary_condition_type
         self.coarse = self.mesh.coarse
 
-        # Setting boundary conditions
-        self.set_boundary_conditions('x')
+        # Coarsening informations
+        self.coarse_config = coarse_config()
+        self.tree = self.coarse_config.tree['Simple']
+        self.get_coarse_informations()
+        self.get_coarse_centers()
 
+        # Setting boundary conditions
+        #self.set_boundary_conditions('x')
+
+        # Coordinate System
+        self.x = np.array([1,0,0]);
+        self.y = np.array([0,1,0]);
+        self.z = np.array([0,0,1]);
 
     def set_boundary_conditions(self, direction):
         """
@@ -50,7 +56,7 @@ class LocalUpscaling:
             3: 'self.periodic_pressure()'        # Periodic pressure
             }
 
-        self.bc = self.boundary_conditions.get(self.boundary_condition_type, "print('Invalid boundary condition')")
+        self.bc = self.boundary_conditions.get(self.boundary_condition_type, "\nprint('Invalid boundary condition')")
 
         exec(self.bc)
 
@@ -69,17 +75,7 @@ class LocalUpscaling:
     def fixed_constant_pressure(self):
         """
         Must return a mesh with boundary conditions set in the specific given volume
-
-        Development using IMPRESS' internal methods iface_neighbors and interfaces_faces -> Incomplete! 
         """
-        aux = self.coarse.elements
-
-        # Z direction
-        #for i in range(len(aux)):
-        #    for j in range(self.ny*self.nz):
-        #        self.mesh.pressure[aux[i].volumes.global_id[aux[i].volumes.all[j+4]]] = 1000
-        #    self.mesh.pressure[i] = 1000
-
 
         print('Fixed constant pressure boundary condition applied')
 
@@ -87,13 +83,15 @@ class LocalUpscaling:
         """
         Must return a mesh with boundary conditions set in the specific given volume
         """
-        print('Fixed linear pressure boundary condition applied')
+
+        print('\nFixed linear pressure boundary condition applied')
 
     def periodic_pressure(self):
         """
         Must return a mesh with boundary conditions set in the specific given volume
         """
-        print('Periodic pressure boundary condition applied')
+
+        print('\nPeriodic pressure boundary condition applied')
 
     def get_coarse_informations(self):
         """
@@ -102,3 +100,31 @@ class LocalUpscaling:
         self.nx = self.tree['nx']
         self.ny = self.tree['ny']
         self.nz = self.tree['nz']
+
+        print('\nCoarsening informations accessed')
+
+    def get_coarse_centers(self):
+        """
+        Calculates the center of a coarse volume and stores it in a IMPRESS variable coarse_center
+        """
+        for i in range(len(self.coarse.elements)):
+            coords = self.coarse.elements[i].nodes.coords[:]
+            x_coords = coords[:,0]
+            y_coords = coords[:,1]
+            z_coords = coords[:,2]
+
+            xmax = np.amax(x_coords)
+            ymax = np.amax(y_coords)
+            zmax = np.amax(z_coords)
+
+            xmin = np.amin(x_coords)
+            ymin = np.amin(y_coords)
+            zmin = np.amin(z_coords)
+
+            x_center = xmin + (xmax-xmin)/2
+            y_center = ymin + (ymax-ymin)/2
+            z_center = zmin + (zmax-zmin)/2
+
+            self.coarse.elements[i].coarse_center[:] = np.array([x_center, y_center, z_center])
+
+        print('\nCoarse volume centers calculated')
