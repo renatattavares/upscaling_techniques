d00f4fd8acd128c66c4762906c0d75eb62466567
Renata <renatattavares@hotmail.com>
2020-01-09 15:21:38 -0300

    Local upscaling initialized

--- local_upscaling.py
+++ local_upscaling.py
@@ -1,5 +1,5 @@
 from impress.preprocessor.meshHandle.multiscaleMesh import FineScaleMeshMS as impress
 from impress.preprocessor.meshHandle.configTools.configClass import coarseningInit as coarse_config
-from upscaling_procedures.local_upscaling import LocalUpscaling
+from upscaling_procedures.local_upscaling import LocalUpscaling, LocalProblems
 
-lu = LocalUpscaling(preprocessor = impress, coarse_config = coarse_config, mesh_file = 'mesh/25.h5m', boundary_condition_type = 1)
+lu = LocalUpscaling(LocalProblems)
