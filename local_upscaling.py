ffbe3e99539bba6629a68b98958f7e6db95eca0b
Renata Tavares <renatattavares@hotmail.com>
2020-01-09 17:19:14 -0300

    Classes are connected. Starting development of local upscaling class officially

--- local_upscaling.py
+++ local_upscaling.py
@@ -1,5 +1,6 @@
 from impress.preprocessor.meshHandle.multiscaleMesh import FineScaleMeshMS as impress
 from impress.preprocessor.meshHandle.configTools.configClass import coarseningInit as coarse_config
-from upscaling_procedures.local_upscaling import LocalUpscaling, LocalProblems
+from upscaling_procedures.local_problems import LocalProblems
+from upscaling_procedures.local_upscaling import LocalUpscaling
 
-lu = LocalUpscaling(LocalProblems)
+lu = LocalUpscaling(local_problems_object = LocalProblems, preprocessor = impress, coarse_config = coarse_config, mesh_file = 'mesh/25.h5m', boundary_condition_type = 1)
