79dbdcb34f19394b60a7211b18c18ffdb233cbd5
Renata Tavares <renatattavares@hotmail.com>
2020-03-12 16:39:39 -0300

    An error is occuring in function identity adjacent volumes to the wall. Check.

--- upscaling_procedures/local/local_upscaling.py
+++ upscaling_procedures/local/local_upscaling.py
@@ -11,6 +11,8 @@ class LocalUpscaling(LocalProblems):
         initial_time = time.time()
 
         super().__init__(mesh_file, dataset)
+        self.center_distance_walls()
+        self.upscale_permeability()
 
         final_time = time.time()
         print("\nThe upscaling lasted {0}s".format(final_time-initial_time))
@@ -23,30 +25,35 @@ class LocalUpscaling(LocalProblems):
         area = 1
         for i in range(self.number_coarse_volumes):
             for j in self.direction_string:
-                direction = self.directions_dictionary.get(j)
+                direction = j
 
-                if direction is 'x':
-                    wall = self.wall_x[i]
+                if direction == 'x':
+                    wall = self.coarse_face_x[i]
                     pressures = self.pressure_x
-                elif direction is 'y':
-                    wall = self.wall_y[i]
+                    center_distance_walls = self.center_distance_walls_x[i]
+                elif direction == 'y':
+                    wall = self.coarse_face_y[i]
                     pressures = self.pressure_y
-                elif direction is 'z':
-                    wall = self.wall_z[i]
+                    center_distance_walls = self.center_distance_walls_y[i]
+                elif direction == 'z':
+                    wall = self.coarse_face_z[i]
                     pressures = self.pressure_z
+                    center_distance_walls = self.center_distance_walls_z[i]
 
                 global_wall = self.coarse.elements[i].volumes.global_id[wall]
-                global_adj = identify_adjacent_volumes_to_wall(wall)
+                global_adj = self.identify_adjacent_volumes_to_wall(i, wall)
                 center_wall = self.coarse.elements[i].volumes.center[wall]
                 center_adj = self.coarse.elements[i].volumes.center[adjacent_volumes]
                 pressure_wall = pressures[global_wall]
                 pressure_adj = pressure[global_adj]
-                permeability_wall = get_absolute_permeabilities(direction, global_wall)
-                permeability_adj = get_absolute_permeabilities(direction, global_adj)
+                permeability_wall = self.get_absolute_permeabilities(direction, global_wall)
+                permeability_adj = self.get_absolute_permeabilities(direction, global_adj)
 
                 flow_rate = (2*np.multiply(permeability_wall,permeability_adj)/(permeability_wall+permeability_adj))*(pressure_wall-pressure_adj)/np.linalg.norm(center_wall - center_adj, axis = 1).sum()
 
-                effective_permeability = 4*self.flow_rate/(area*self.number_faces_coarse_face)
+                effective_permeability = center_distance_walls[i]*flow_rate/(area*self.number_faces_coarse_face)
+
+                print(effective_permeability)
 
     def upscale_porosity(self):
         """
@@ -59,22 +66,35 @@ class LocalUpscaling(LocalProblems):
             volumes = np.ones(len(porosity))
             effective_porosity = (np.multiply(porosity*volumes).sum())/volumes.sum()
 
-    def identify_adjacent_volumes_to_wall(self, wall):
+    def identify_adjacent_volumes_to_wall(self, coarse_volume, wall):
 
-        adjacent_volumes = self.coarse.elements[i].volumes.bridge_adjacencies(wall, 2, 3).flatten()
-        repeated_volumes = np.isin(adjacent, wall, invert = True)
-        adjacent_volumes = np.unique(adjacent_volumes[repeated_volumes])
-        global_id_adjacent_volumes = self.coarse.elements[i].volumes.global_id[adjacent_volumes]
+        adjacent_volumes = self.coarse.elements[coarse_volume].volumes.bridge_adjacencies(wall, 2, 3).flatten()
+        repeated_volumes = np.isin(adjacent_volumes, wall, invert = True).flatten()
+        adjacent_volumes = np.unique(adjacent_volumes[repeated_volumes].flatten())
+        global_id_adjacent_volumes = self.coarse.elements[coarse_volume].volumes.global_id[adjacent_volumes]
 
         return global_id_adjacent_volumes
 
     def get_absolute_permeabilities(self, direction, global_ids):
 
-        if direction is 'x':
+        if direction == 'x':
             permeability = self.mesh.permeability[global_ids][0]
-        elif direction is 'y':
+        elif direction == 'y':
             permeability = self.mesh.permeability[global_ids][1]
-        elif direction is 'z':
+        elif direction == 'z':
             permeability = self.mesh.permeability[global_ids][2]
 
         return permeability
+
+    def center_distance_walls(self):
+
+        self.center_distance_walls_x = np.array([])
+        self.center_distance_walls_y = np.array([])
+        self.center_distance_walls_z = np.array([])
+
+        for i in range(self.number_coarse_volumes):
+            min_coord = self.coarse.elements[i].volumes.center[:].min(axis = 0)
+            max_coord = self.coarse.elements[i].volumes.center[:].min(axis = 0)
+            self.center_distance_walls_x = np.append(self.center_distance_walls_x, (min_coord[0], max_coord[0]))
+            self.center_distance_walls_y = np.append(self.center_distance_walls_x, (min_coord[1], max_coord[1]))
+            self.center_distance_walls_z = np.append(self.center_distance_walls_x, (min_coord[2], max_coord[2]))
