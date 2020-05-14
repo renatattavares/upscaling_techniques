f17f5ad5840aa08cb8bd7eed98446018e4460af2
Renata Tavares <renatattavares@hotmail.com>
2020-05-14 16:04:33 -0300

    Function to identify and write heading on coarse dataset

--- local.py
+++ local.py
@@ -1,9 +1,10 @@
-from imex_integration.read_dataset import read_dataset
-from upscaling_procedures.local.local_problems import LocalProblems
-from upscaling_procedures.local.local_upscaling import LocalUpscaling
-from upscaling_procedures.local.parallel_local_problems import ParallelLocalProblems
-from upscaling_procedures.local.parallel_local_upscaling import ParallelLocalUpscaling
-from impress.preprocessor.meshHandle.multiscaleMesh import FineScaleMeshMS as impress
+#from imex_integration.read_dataset import read_dataset
+from imex_integration.write_dataset import DatasetWriter
+#from upscaling_procedures.local.local_problems import LocalProblems
+#from upscaling_procedures.local.local_upscaling import LocalUpscaling
+#from upscaling_procedures.local.parallel_local_problems import ParallelLocalProblems
+#from upscaling_procedures.local.parallel_local_upscaling import ParallelLocalUpscaling
+#from impress.preprocessor.meshHandle.multiscaleMesh import FineScaleMeshMS as impress
 
 ############### RUN LOCAL PROBLEMS ###############
 #lp = LocalProblems(mesh_file = 'mesh/20.h5m', dataset = None)
@@ -18,7 +19,10 @@ from impress.preprocessor.meshHandle.multiscaleMesh import FineScaleMeshMS as im
 #lu = ParallelLocalUpscaling(mesh_file = None, dataset ='imex_datasets/new_super.dat')
 
 ############### READ DATASET ###############
-#porosity, permeability = read_dataset('imex_datasets/spe_10_case_2/spe_10_case_2.dat', 'mesh/generated_mesh.h5m'    )
+#porosity, permeability = read_dataset('imex_datasets/spe_10_case_2/spe_10_case_2.dat')
+
+############### WRITE DATASET ###############
+new_dataset = DatasetWriter('imex_datasets/spe_10_case_2/spe_10_case_2.dat')
 
 ############### RUN IMPRESS ###############
 #mesh = impress(mesh_file = 'generated_mesh.h5m', dim = 3)
