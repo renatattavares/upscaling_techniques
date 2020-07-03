fa78f71cf53391fb54009142dab60da50096cf88
Renata Tavares <renatattavares@hotmail.com>
2020-07-03 17:30:42 -0300

    Updating impress, docker image and travis file.

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ../home && ls -a
-cd ../elliptic_case && python3 -m unittest code_tests/test_20_case.py
+cd ../home/scientific && ./pymoab_update.sh
+cd ../../test && python3 -m unittest code_tests/test_20_case.py
