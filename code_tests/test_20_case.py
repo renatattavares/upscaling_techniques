bbca1e2554ab96468f0517e713c6f2ab0f124cce
Renata Tavares <renatattavares@hotmail.com>
2020-07-03 18:10:23 -0300

    Add features to test script

--- code_tests/test_20_case.py
+++ code_tests/test_20_case.py
@@ -4,11 +4,17 @@ from upscaling_procedures.local.parallel_local_upscaling import ParallelLocalUps
 
 class Test20Case(unittest.TestCase):
 
-    def test_effective_permeability(self):
+    def setup(self):
 
         self.lu = ParallelLocalUpscaling(mesh_file = 'mesh/20.h5m', dataset = None)
         self.lu.save_info()
 
+    def test_effective_permeability(self):
+
         self.assertEqual(self.lu.effective_permeability[:,0].all(), np.ones(125).all())
         self.assertEqual(self.lu.effective_permeability[:,1].all(), np.ones(125).all())
         self.assertEqual(self.lu.effective_permeability[:,2].all(), np.ones(125).all())
+
+    def test_effective_porosity(self):
+
+        self.assertEqual(self.lu.test_effective_porosity.all(), np.ones(125).all())
--- run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../home/scientific && ./pymoab_update.sh
-cd ../../test && python3 -m unittest code_tests/test_20_case.py
